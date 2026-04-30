1e6709067512d1d91d20f588909857137843e95e
guirinaldi15 <tremmbala45@gmail.com>
2026-04-30 10:27:05 -0300

    Crud Atualizado

--- app/Livewire/Produto/ProdutoEdit.php
+++ app/Livewire/Produto/ProdutoEdit.php
@@ -12,6 +12,7 @@ class ProdutoEdit extends Component
     public $valor;
     public $qtd_estoque;
     public $qtd_minima;
+
     public function mount($id)
     {
         $produto = Produto::find($id);
@@ -25,6 +26,24 @@ class ProdutoEdit extends Component
         $this->qtd_estoque = $produto->qtd_estoque;
         $this->qtd_minima = $produto->qtd_minima;
     }
+
+    public function update()
+    {
+        $produto = Produto::find($this->id);
+        if ($produto == null) {
+            session()->flash('error', 'Produto não encontrado!');
+            return redirect()->route('produto.index');
+        }
+        $produto->id = $this->id;
+        $produto->nome = $this->nome;
+        $produto->valor = $this->valor;
+        $produto->qtd_estoque = $this->qtd_estoque;
+        $produto->qtd_minima = $this->qtd_minima;
+        $produto->save();
+        session()->flash('success', 'Produto atualizado com sucesso!');
+        return redirect()->route('produto.index');
+    }
+    
     public function render()
     {
         return view('livewire.produto.produto-edit');
--- resources/views/livewire/produto/produto-index.blade.php
+++ resources/views/livewire/produto/produto-index.blade.php
@@ -1,4 +1,20 @@
 <div class='mt-5'>
+    @if (session()->has('error'))
+        <div class="alert alert-danger">
+            {{ session('error') }}
+        </div>
+    @endif
+
+    @if (session()->has('success'))
+        <div class="alert alert-success">
+            {{ session('success') }}
+        </div>
+    @endif
+
+    <div class="mb-3">
+        <input type="text" wire:model.live='search' placeholder="Pesquisar...." class="form-control">
+    </div>
+
     <table class="table table-hover">
         <thead>
             <tr>
@@ -7,17 +23,23 @@
                 <th scope="col">Valor</th>
                 <th scope="col">Qtd. Estoque</th>
                 <th scope="col">Qtd. Mínima</th>
+                <th scope="col">Ações</th>
             </tr>
         </thead>
         <tbody>
-            @foreach($produtos as $p)
-            <tr>
-                <th scope="row">{{ $p->id }}</th>
-                <td>{{ $p->nome }}</td>
-                <td>{{ $p->valor }}</td>
-                <td>{{ $p->qtd_estoque }}</td>
-                <td>{{ $p->qtd_minima }}</td>
-            </tr>
+            @foreach ($produtos as $p)
+                <tr>
+                    <th scope="row">{{ $p->id }}</th>
+                    <td>{{ $p->nome }}</td>
+                    <td>{{ $p->valor }}</td>
+                    <td>{{ $p->qtd_estoque }}</td>
+                    <td>{{ $p->qtd_minima }}</td>
+                    <td>
+                        <a href="{{ route('produto.edit', ['id' => $p->id]) }}" class="btn btn-sm btn-info">Editar</a>
+
+                        <button wire:click='delete({{ $p->id }})' class="btn btn-sm btn-danger">Excluir</button>
+                    </td>
+                </tr>
             @endforeach
         </tbody>
     </table>
